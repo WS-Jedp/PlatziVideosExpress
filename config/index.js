37bc0c52a3d8a392df58904f4b6ffd7964e9ae98
Jedp <jedp082@gmail.com>
2020-04-23 22:09:43 -0500

    Configurations fixed

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const config = {
-  dev: proccess.env.NODE_ENV !== 'production',
+  dev: process.env.NODE_ENV !== 'production',
   port: process.env.PORT || 3000
 };
 
